524cfb400c176435d71599d64ea0adaad3fa361b
JinGoo <zzgg3040@naver.com>
2025-05-19 15:18:13 +0900

    refactor: change-furniture-category-policy

--- src/furniture/constant/furniture-seed-data.ts
+++ src/furniture/constant/furniture-seed-data.ts
@@ -7,7 +7,7 @@ export const FURNITURE_SEEDS: DeepPartial<Furniture>[] = [
     code: '20250524-00',
     name: '기본 바닥재',
     price: 0,
-    category: FurnitureCategory.WALL_PAPER,
+    category: FurnitureCategory.FLOOR,
     zIndex: 0,
     s3ImageUrl:
       'https://regreen-bucket.s3.ap-northeast-2.amazonaws.com/images/constant/furniture/20250524-00.png',
@@ -17,9 +17,9 @@ export const FURNITURE_SEEDS: DeepPartial<Furniture>[] = [
   {
     code: '20250524-01',
     name: '기본 벽지',
-    price: 5,
+    price: 0,
     category: FurnitureCategory.WALL_PAPER,
-    zIndex: 1,
+    zIndex: 5,
     s3ImageUrl:
       'https://regreen-bucket.s3.ap-northeast-2.amazonaws.com/images/constant/furniture/20250524-01.png',
     s3PreviewImageUrl:
@@ -27,10 +27,10 @@ export const FURNITURE_SEEDS: DeepPartial<Furniture>[] = [
   },
   {
     code: '20250524-02',
-    name: '에코 선반 세트',
-    price: 150,
-    category: FurnitureCategory.FURNITURE,
-    zIndex: 6,
+    name: '에코 벽지',
+    price: 5,
+    category: FurnitureCategory.WALL_PAPER,
+    zIndex: 10,
     s3ImageUrl:
       'https://regreen-bucket.s3.ap-northeast-2.amazonaws.com/images/constant/furniture/20250524-02.png',
     s3PreviewImageUrl:
@@ -38,10 +38,10 @@ export const FURNITURE_SEEDS: DeepPartial<Furniture>[] = [
   },
   {
     code: '20250524-03',
-    name: '에코 캘린더',
-    price: 80,
-    category: FurnitureCategory.PROP,
-    zIndex: 11,
+    name: '에코 선반 세트',
+    price: 150,
+    category: FurnitureCategory.FURNITURE,
+    zIndex: 15,
     s3ImageUrl:
       'https://regreen-bucket.s3.ap-northeast-2.amazonaws.com/images/constant/furniture/20250524-03.png',
     s3PreviewImageUrl:
@@ -49,10 +49,10 @@ export const FURNITURE_SEEDS: DeepPartial<Furniture>[] = [
   },
   {
     code: '20250524-04',
-    name: '에코 창문',
-    price: 60,
+    name: '에코 캘린더',
+    price: 80,
     category: FurnitureCategory.PROP,
-    zIndex: 16,
+    zIndex: 20,
     s3ImageUrl:
       'https://regreen-bucket.s3.ap-northeast-2.amazonaws.com/images/constant/furniture/20250524-04.png',
     s3PreviewImageUrl:
@@ -60,10 +60,10 @@ export const FURNITURE_SEEDS: DeepPartial<Furniture>[] = [
   },
   {
     code: '20250524-05',
-    name: '에코 러그',
-    price: 200,
+    name: '에코 창문',
+    price: 60,
     category: FurnitureCategory.WINDOW,
-    zIndex: 21,
+    zIndex: 25,
     s3ImageUrl:
       'https://regreen-bucket.s3.ap-northeast-2.amazonaws.com/images/constant/furniture/20250524-05.png',
     s3PreviewImageUrl:
@@ -71,10 +71,10 @@ export const FURNITURE_SEEDS: DeepPartial<Furniture>[] = [
   },
   {
     code: '20250524-06',
-    name: '에코 쿠션',
-    price: 120,
+    name: '에코 러그',
+    price: 200,
     category: FurnitureCategory.PROP,
-    zIndex: 26,
+    zIndex: 30,
     s3ImageUrl:
       'https://regreen-bucket.s3.ap-northeast-2.amazonaws.com/images/constant/furniture/20250524-06.png',
     s3PreviewImageUrl:
@@ -82,10 +82,10 @@ export const FURNITURE_SEEDS: DeepPartial<Furniture>[] = [
   },
   {
     code: '20250524-07',
-    name: '에코 동화',
-    price: 90,
+    name: '에코 쿠션',
+    price: 120,
     category: FurnitureCategory.PROP,
-    zIndex: 31,
+    zIndex: 35,
     s3ImageUrl:
       'https://regreen-bucket.s3.ap-northeast-2.amazonaws.com/images/constant/furniture/20250524-07.png',
     s3PreviewImageUrl:
@@ -93,10 +93,10 @@ export const FURNITURE_SEEDS: DeepPartial<Furniture>[] = [
   },
   {
     code: '20250524-08',
-    name: '에코 침대',
-    price: 50,
+    name: '에코 동화책',
+    price: 90,
     category: FurnitureCategory.PROP,
-    zIndex: 36,
+    zIndex: 40,
     s3ImageUrl:
       'https://regreen-bucket.s3.ap-northeast-2.amazonaws.com/images/constant/furniture/20250524-08.png',
     s3PreviewImageUrl:
@@ -104,10 +104,10 @@ export const FURNITURE_SEEDS: DeepPartial<Furniture>[] = [
   },
   {
     code: '20250524-09',
-    name: '에코 스탠드',
-    price: 500,
+    name: '에코 침대',
+    price: 50,
     category: FurnitureCategory.FURNITURE,
-    zIndex: 41,
+    zIndex: 45,
     s3ImageUrl:
       'https://regreen-bucket.s3.ap-northeast-2.amazonaws.com/images/constant/furniture/20250524-09.png',
     s3PreviewImageUrl:
@@ -115,10 +115,10 @@ export const FURNITURE_SEEDS: DeepPartial<Furniture>[] = [
   },
   {
     code: '20250524-10',
-    name: '에코 가방',
-    price: 250,
+    name: '에코 스탠드',
+    price: 500,
     category: FurnitureCategory.PROP,
-    zIndex: 46,
+    zIndex: 50,
     s3ImageUrl:
       'https://regreen-bucket.s3.ap-northeast-2.amazonaws.com/images/constant/furniture/20250524-10.png',
     s3PreviewImageUrl:
@@ -126,10 +126,10 @@ export const FURNITURE_SEEDS: DeepPartial<Furniture>[] = [
   },
   {
     code: '20250524-11',
-    name: '에코 침낭',
-    price: 120,
+    name: '에코 가방',
+    price: 250,
     category: FurnitureCategory.PROP,
-    zIndex: 51,
+    zIndex: 55,
     s3ImageUrl:
       'https://regreen-bucket.s3.ap-northeast-2.amazonaws.com/images/constant/furniture/20250524-11.png',
     s3PreviewImageUrl:
@@ -140,7 +140,7 @@ export const FURNITURE_SEEDS: DeepPartial<Furniture>[] = [
     name: '에코 책상 세트',
     price: 300,
     category: FurnitureCategory.FURNITURE,
-    zIndex: 56,
+    zIndex: 60,
     s3ImageUrl:
       'https://regreen-bucket.s3.ap-northeast-2.amazonaws.com/images/constant/furniture/20250524-12.png',
     s3PreviewImageUrl:
@@ -151,7 +151,7 @@ export const FURNITURE_SEEDS: DeepPartial<Furniture>[] = [
     name: '에코 의자',
     price: 100,
     category: FurnitureCategory.FURNITURE,
-    zIndex: 61,
+    zIndex: 65,
     s3ImageUrl:
       'https://regreen-bucket.s3.ap-northeast-2.amazonaws.com/images/constant/furniture/20250524-13.png',
     s3PreviewImageUrl:
