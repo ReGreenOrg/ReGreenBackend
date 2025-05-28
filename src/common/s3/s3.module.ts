b81cca0909b8811cc4bfac850520607480151f8f
JinGoo <zzgg3040@naver.com>
2025-05-29 07:57:14 +0900

    hotfix: fix-filesize-limit

--- src/common/s3/s3.module.ts
+++ src/common/s3/s3.module.ts
@@ -5,6 +5,8 @@ import { MulterOptions } from '@nestjs/platform-express/multer/interfaces/multer
 import * as multerS3 from 'multer-s3';
 import { S3Client } from '@aws-sdk/client-s3';
 import { basename, extname } from 'path';
+import { BusinessException } from '../exception/business-exception';
+import { ErrorType } from '../exception/error-code.enum';
 
 @Module({
   imports: [
@@ -34,7 +36,23 @@ import { basename, extname } from 'path';
             callback(null, fileName);
           },
         }),
-        limits: {},
+        fileFilter: (req, file, cb) => {
+          const allowed = ['image/png', 'image/jpeg', 'image/webp'];
+          if (allowed.includes(file.mimetype)) {
+            cb(null, true);
+          } else {
+            cb(
+              new BusinessException(
+                ErrorType.INVALID_FILE_FORMAT,
+                'Only PNG, JPEG, and WEBP formats under 20 MB are allowed.',
+              ),
+              false,
+            );
+          }
+        },
+        limits: {
+          fileSize: 15 * 1024 * 1024, // 15MB
+        },
       }),
     }),
   ],
