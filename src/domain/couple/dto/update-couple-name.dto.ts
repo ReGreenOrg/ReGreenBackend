dfe28e74f9711f4aaac6693f189acd392bca6d89
JinGoo <zzgg3040@naver.com>
2025-06-17 22:10:04 +0900

    fix: Reduce-string-check-conditions

--- src/domain/couple/dto/update-couple-name.dto.ts
+++ src/domain/couple/dto/update-couple-name.dto.ts
@@ -3,12 +3,13 @@ import { IsNotEmpty, IsString, Length, Matches } from 'class-validator';
 export class UpdateCoupleNameDto {
   @IsString()
   @IsNotEmpty()
-  @Length(1, 12, {
+  @Length(1, 10, {
     message:
       'The name must be at least $constraint1 characters and at most $constraint2 characters. House name',
   })
-  @Matches(/^[가-힣a-zA-Z0-9 ]+$/, {
-    message: 'The name can only contain Korean, English, numbers, and spaces.',
+  @Matches(/^[\p{L}\p{N}\p{P}\p{S} ]+$/u, {
+    message:
+      'The name can contain any letters, numbers, punctuation, symbols, and spaces.',
   })
   name: string;
 }
